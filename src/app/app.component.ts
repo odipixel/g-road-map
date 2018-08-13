93d24e32a739776f00fe3c73574ab21021f2f62a
kishoremallick <kishore.mallick@incedoinc.com>
2018-08-13 16:46:57 +0530

    map added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'g-map-road';
+   title: string = 'Google direction api';
+ public lat: Number = 24.799448
+public lng: Number = 120.979021
+ 
+public origin: {}
+public destination: {}
+ngOnInit() {
+  this.getDirection()
+}
+getDirection() {
+  this.origin = { lat: 24.799448, lng: 120.979021 }
+  this.destination = { lat: 24.799524, lng: 120.975017 }
+ 
+  // this.origin = 'Taipei Main Station'
+  // this.destination = 'Taiwan Presidential Office'
+}
+
+  public onChangeStart(event): void {  // event will give you full breif of action
+    const newVal = event.target.value;
+    this.origin = newVal
+  
+}
+
+ public onChangeEnd(event): void {  // event will give you full breif of action
+    const newVal = event.target.value;
+    
+  this.destination = newVal
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,ApplicationRef} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction' 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDPOGxBB9AxeulwqE7AM2uUNq_jjFmk6_Y'
+    }),
+    AgmDirectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
