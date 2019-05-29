def11f106e0a6df4454f7a803189cf2fd6f37869
kishore mallick <kishore.mallick@outlook.com>
2019-05-29 14:50:49 +0530

    styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   title: string = 'Google direction api';
+   title: string = 'Google direction map';
 public lat: Number = 12.9698
 public lng: Number = 77.7499
  
